18b7ba10877bee2846e9a48a4716184993610d7d
Bijoya-Mondal <66239075+Bijoya-Mondal@users.noreply.github.com>
2025-02-26 21:08:27 +0530

    Update index.js

--- index.js
+++ index.js
@@ -11,6 +11,12 @@ app.get('/docker', (req, res) => {
     res.send('DOCKERIZE THIS !!!!!!!!!!!!!')
   })
 
+app.get('/sendParam', (req,res)=>{
+  const token = req.query.token
+  console.log(token)
+  res.send(`the following param was sent: ${token}`)
+  })
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
